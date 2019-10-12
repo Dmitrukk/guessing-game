abb6262cb6c027041c5e0c2ee91b94b75ed0752a
Dmitrukk <d.dmitrukk@gmail.com>
2019-10-13 01:04:26 +0300

    Commit No.1

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        
+        return Math.round((this.min+this.max)/2);
     }
 
     lower() {
-
+        this.setRange(this.min, Math.round((this.min + this.max)/2));
     }
 
     greater() {
-
+        this.setRange(Math.round((this.min + this.max)/2), this.max);
     }
 }
 
